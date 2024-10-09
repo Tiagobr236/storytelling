85d2d25ec91bf7cf25df182b55c56705b1e508bc
hiroki007 <125603333+Tiagobr236@users.noreply.github.com>
2024-10-09 16:54:22 -0300

    tuiagio

---
+++ graficos/informacoesGlobais.js
@@ -0,0 +1,18 @@
+const url + 'https://raw.githubusercontent.com/tiagoyamashita/api/main/dados-globais'
+
+async function vizualizarInformacoesGlobais() {
+    const res = await fetch (url)
+    const dados = await res.json()
+    const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
+    const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
+    const horas = parseInt(dados.tempo_medio)
+    const minutos = Math.round((dados.tempo_medio - horas) * 100)
+    const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo ) * 100).toFixed
+
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões
+
+    const container = document.createElementById('graficos-container')
+    
+}
